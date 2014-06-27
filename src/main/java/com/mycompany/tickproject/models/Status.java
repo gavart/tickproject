1db8f18bd1a5fdeaf6d2a87fa641a543009b06e9
jenias <jenia.strebkov@gmail.com>
2014-06-27 18:51:59 +0300

    add entity Status

---
+++ src/main/java/com/mycompany/tickproject/models/Status.java
@@ -0,0 +1,37 @@
+package com.mycompany.tickproject.models;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+/**
+ * Created by apple on 27.06.14.
+ */
+
+@Entity
+@Table(name = "Statuses")
+public class Status {
+
+    //properties
+    private int id;
+    private String Status;
+
+    //getters
+    @Column(name = "id")
+    public int getId() {
+        return id;
+    }
+    @Column(name = "status")
+    public String getStatus() {
+        return Status;
+    }
+
+    //setters
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setStatus(String status) {
+        Status = status;
+    }
+}

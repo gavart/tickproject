df3470d6e937b15c0676b47d7201e276d3a1adb8
Alexander Tverdokhleb <batone@ukr.net>
2014-07-24 15:01:34 +0300

    Revert "Add charset settings in web.xml"
    This reverts commit 6ee978846b9434c8e135ae2e2419734d26884241.

--- src/main/java/com/mycompany/tickproject/controllers/ActionController.java
+++ src/main/java/com/mycompany/tickproject/controllers/ActionController.java
@@ -53,7 +53,7 @@ public class ActionController {
         action.setIsActive(true);
         action.setDateTimeAction(Timestamp.valueOf(dateAction));
 
-        facadeService.getActionService().addAction(action);
+        //facadeService.getActionService().addAction(action);
         Action lastAddedAction = facadeService.getActionService().getLastAddedAction();
         if(isNewPrices==false) {
             List<Price> defaultPrices = facadeService.getPriceService().getPrices(1);//default actionID = 1
@@ -62,7 +62,7 @@ public class ActionController {
                 newPrice.setSectionOfStadium(price.getSectionOfStadium());
                 newPrice.setAction(lastAddedAction);
                 newPrice.setPrice(price.getPrice());
-                facadeService.getPriceService().addPrice(newPrice);
+                //facadeService.getPriceService().addPrice(newPrice);
             }
         } else {
             for(int i=0; i < prices.size(); i++ ) {
@@ -72,7 +72,7 @@ public class ActionController {
                 newPrice.setSectionOfStadium(sectionOfStadium);
                 newPrice.setAction(lastAddedAction);
                 newPrice.setPrice(prices.get(i));
-                facadeService.getPriceService().addPrice(newPrice);
+                //facadeService.getPriceService().addPrice(newPrice);
             }
 
         }

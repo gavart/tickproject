b230253e564a7fb5730a53eab47a2a98def6714b
jenias <jenia.strebkov@gmail.com>
2014-07-28 15:49:22 +0300

    added some fields to model Action, changed some files

--- src/main/java/com/mycompany/tickproject/controllers/SearchController.java
+++ src/main/java/com/mycompany/tickproject/controllers/SearchController.java
@@ -1,53 +1,29 @@
 package com.mycompany.tickproject.controllers;
 
-<<<<<<< HEAD
 import com.mycompany.tickproject.models.Price;
-=======
+
 import com.mycompany.tickproject.models.Customer;
->>>>>>> origin/master
 import com.mycompany.tickproject.service.FacadeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-<<<<<<< HEAD
-import org.springframework.web.portlet.ModelAndView;
-=======
 import org.springframework.web.bind.annotation.ResponseBody;
->>>>>>> origin/master
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.util.List;
 
 /**
-<<<<<<< HEAD
- * Created by tverd.alex on 24.07.2014.
- */
-@Controller
-public class SearchController {
-
-=======
  * Created by apple on 27.07.14.
  */
 @Controller
 public class SearchController {
->>>>>>> origin/master
     @Autowired
     private FacadeService facadeService;
 
     @RequestMapping(value = "/search", method = RequestMethod.GET)
-<<<<<<< HEAD
-    public ModelAndView search(HttpServletRequest request, HttpServletResponse response, Model map) {
-        ModelAndView modelAndView = new ModelAndView("search");
-
-
-
-        return modelAndView;
-    }
-
-=======
     public String searchPage(HttpServletRequest request, HttpServletResponse response,Model map) {
         String msg = "";
         String typemsg = "";
@@ -91,5 +67,4 @@ public class SearchController {
 
         return "searchp";
     }
->>>>>>> origin/master
 }
